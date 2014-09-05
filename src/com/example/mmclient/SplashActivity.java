cb5517238c174f8470139240f60ebfb6a706f077
swapnil <swapnil.raipur@gmail.com>
2014-09-05 10:34:52 +0530

    Added Splash and background image

---
+++ src/com/example/mmclient/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.mmclient;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.mmclient.activity.MainActivity;
+
+/**
+ * Activity to manage Splash effect.
+ * 
+ * @author SWAPNIL
+ * 
+ */
+public class SplashActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		// Adding a delay of 1000 milliseconds after which calling MainActivity.
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent mainIntent = new Intent(SplashActivity.this,
+						MainActivity.class);
+				startActivity(mainIntent);
+				finish();
+			}
+		}, 1000);
+	}
+}
